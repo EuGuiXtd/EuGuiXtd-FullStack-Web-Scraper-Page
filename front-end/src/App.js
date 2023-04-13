5d727a3b68b2e60cb1376817de8907120e833745
EuGuiXtd <guilhermedpjf@gmail.com>
2023-04-13 14:09:07 -0300

    feat[base of front]: add a base for front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { getProductsAndPostSearch } from './services/request';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  const [selectCategory, setSelectCategory] = useState('geladeira');
+
+  const [selectSite, setSelectSite] = useState('Mercado Livre');
+
+  const [inputProduct, setInputProduct] = useState('');
+  
+  const [researched, setResearched] = useState(false)
+
+  const handleInputProduct = ({ target }) => {
+    setInputProduct(target.value);
+  };
+
+
+
+  const formValidation = () => {
+    if(inputProduct === '' || selectCategory === '' || selectSite === ''){
+      return true;
+    }
+  }
+
+  const newSale = async (event) => {
+    event.preventDefault();
+    try {
+      const data = await getProductsAndPostSearch(
+        {
+          search: inputProduct, 
+          site: selectSite, 
+          category: selectCategory,
+        });
+      setProducts(data);
+      setResearched(true)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(products);
+  console.log(selectCategory);
+  console.log(selectSite);
+  console.log(inputProduct);
+  console.log(researched);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select
+        value={ selectCategory }
+        onChange={ (e) => setSelectCategory(e.target.value) }
+      >
+          <option 
+          value={ (selectSite === 'Mercado Livre' || selectSite === 'Buscapé')  ? 'celular' : '' }
+          >
+            Mobile
+            </option>
+          <option 
+          value={ (selectSite === 'Mercado Livre' || selectSite === 'Buscapé') ? 'geladeira' : '' }
+          >
+            Refrigerator
+            </option>
+          <option 
+          value={ (selectSite === 'Mercado Livre' || selectSite === 'Buscapé') ? 'tv' : '' }
+          >
+            TV
+            </option>
+      </select>
+      <select
+        value={ selectSite }
+        onChange={ (e) => setSelectSite(e.target.value) }
+      >
+          <option 
+          value='Mercado Livre'
+          >
+            Mercado Livre
+            </option>
+          <option 
+          value='Buscapé'
+          >
+            Buscapé
+            </option>
+      </select>
+      <label 
+      htmlFor='search-for-products'
+      >
+      <input 
+      id='search-for-products' 
+      type='text' 
+      placeholder='Search for products here'
+      value={ inputProduct }
+      onChange={ handleInputProduct }
+      />
+      </label>
+      <button 
+      type='button'
+      onClick={ (event) => newSale(event) }
+      disabled={formValidation()}
+      >
+        Search
+        </button>
+{/*         {
+        (researched)
+          ? (
+            products.results.map((product) => (
+              <section
+              key={ product.id }
+              >
+                {product.price}
+                <a href={product.permalink}>{product.permalink}</a>
+                <img src={product.thumbnail} alt='product thumbnail' />
+                </section>
+            ))
+          )
+          : null
+      } */}
     </div>
   );
 }
---
+++ front-end/src/services/request.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
+});
+
+export const getProductsAndPostSearch = async ({ search, site, category }) => {
+  const { data } = await api.get('/products', { search, site, category });
+
+  return data;
+};
+
+export default api;
